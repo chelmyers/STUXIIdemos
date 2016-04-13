68f4d1a6ce18113ea1c94a94a5568dcb1da6a38e
Chelsea Myers <chel.myers@gmail.com>
2016-04-13 05:48:20 -0400

    save name through LS

--- wk03/ex02/a/js/app.js
+++ wk03/ex02/a/js/app.js
@@ -1,11 +1,25 @@
-$('#pin-button').click( function() {
-	addMarker(0,0);
-});
+// Your app's JS goes here
 
-$('#info').click( function() {
-	$("#app-info").css("display", "inherit");
-});
+//Wait until HTML is loaded
+window.onload = function() {
 
-$('#close').click( function() {
-	$("#app-info").css("display", "none");
-});
+	var nameSpace = document.getElementsByClassName("user-name")[0];
+	var nameInput = document.getElementsByClassName("user-name-input")[0];
+	var nameSave  = document.getElementsByClassName("user-name-save")[0];
+
+	nameSave.onclick = function(event) {
+		event.preventDefault();
+
+		localStorage.setItem("user-name", nameInput.value);
+
+		nameSpace.innerHTML = nameInput.value;
+	}
+
+	if(localStorage.length > 0){
+		nameSpace.innerHTML = localStorage.getItem("user-name");
+	}
+
+	//// Clear localStorage
+	// localStorage.clear();
+
+};
