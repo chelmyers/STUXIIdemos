a4f23de5566bbbdcef35cf18d082f9197b363dc5
Chelsea Myers <chel.myers@gmail.com>
2016-04-13 16:33:27 -0400

    add cart example

--- wk03/ex02/a/js/app.js
+++ wk03/ex02/a/js/app.js
@@ -1,36 +1,71 @@
 // Your app's JS goes here
 
 //Wait until HTML is loaded
-window.onload = function() {
+$(document).ready(function(){
+	var menuItems = $(".menu-item");
+	var totalCell = $(".total");
+	var LSCart;
 
-	var menuItems = document.getElementsByClassName("menu-item");
-	var totalCell = document.getElementsByClassName("total")[0];
-
-	// cart
+	// cart array structure
 	// -item
-	// --name
-	// --price
-	// --quanitity
+	// --name 0
+	// --price 1
+	// --quanitity 2
 
 
-	menuItems.onclick = function(event) {
-		var itemName = this.dataset.menuItem;
-		var itemPrice = this.dataset.menuPrice;
+	menuItems.click(function() {
+		var itemName = $(this).data('menu-item');
+		var itemPrice = $(this).data('menu-price');
+		var itemQuantity = 1;
+		LSCart = [];
 
-		console.log(itemName + itemPrice);
+		$('.cart').html('<tr><td>Total</td><td></td><td class="total"></td></tr>');
 
-		//var cartArray = [this.dataset.menuItem, this.dataset.menuItem;
+		if(localStorage.getItem("cart")){
+			LSCart = JSON.parse(localStorage.getItem("cart"));
+		}
 
-		//localStorage.setItem("user-name", nameInput.value);
+		for(var i = 0; i < LSCart.length; i++){
+			if(LSCart[i][0] == itemName){
+				itemQuantity += LSCart[i][2];
+				LSCart.splice(i, 1);
+			}
+		}
 
+		var item = [itemName, itemPrice, itemQuantity];
 
+		LSCart.push(item);
+
+		localStorage.setItem("cart", JSON.stringify(LSCart));
+
+		populateCart(LSCart, totalCell);
+
+	});
+
+	if(localStorage.getItem("cart")){
+		LSCart = JSON.parse(localStorage.getItem("cart"));
+		populateCart(LSCart, totalCell);
 	}
 
-	if(localStorage.length > 0){
-		//nameSpace.innerHTML = localStorage.getItem("user-name");
+
+
+});
+
+function populateCart(array, total){
+
+	var totalCost = 0;
+
+	for(var i = 0; i < array.length; i++){
+		$('.cart').prepend("<tr><td>" + array[i][0]
+											+ "</td><td>" + array[i][2]
+											+ "</td><td>" + (array[i][2]* array[i][1])
+											+ "</td></tr>");
+
+		totalCost += (array[i][2] * array[i][1]);
+	  total.html(totalCost);
 	}
+}
 
-	//// Clear localStorage
-	//localStorage.clear();
 
-};
+//// Clear localStorage
+//localStorage.clear();
