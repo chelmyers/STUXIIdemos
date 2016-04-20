f71d001a2648b430cfd4f7b68249a4169c84dce1
Chelsea Myers <chel.myers@gmail.com>
2016-04-20 15:38:02 -0400

    update wk 04 with localstorage

---
+++ wk04/ex01/a/js/app.js
@@ -0,0 +1,27 @@
+// Your app's JS goes here
+
+//Wait until HTML is loaded
+window.onload = function() {
+
+	var nameSpace = document.getElementsByClassName("user-name")[0];
+	var nameInput = document.getElementsByClassName("user-name-input")[0];
+	var nameSave  = document.getElementsByClassName("user-name-save")[0];
+
+	// var element = document.getElementById(id);
+
+	nameSave.onclick = function(event) {
+		event.preventDefault();
+
+		//localStorage.setItem("user-name", nameInput.value);
+
+		nameSpace.innerHTML = nameInput.value;
+	}
+
+	if(localStorage.length > 0){
+		//nameSpace.innerHTML = localStorage.getItem("user-name");
+	}
+
+	//// Clear localStorage
+	//localStorage.clear();
+
+};
