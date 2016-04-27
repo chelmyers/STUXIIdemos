82f4d3348dd4f59e2c14dba5a33466ade2688f64
Chelsea Myers <chel.myers@gmail.com>
2016-04-27 13:09:29 -0400

    motion: test with acx

--- wk05/04-motion/a/js/app.js
+++ wk05/04-motion/a/js/app.js
@@ -1,15 +1,9 @@
-//openweathermap
-//http://openweathermap.org/current#geo
-
-//pretty Print
-//http://jsonprettyprint.com/
+//https://developer.mozilla.org/en-US/docs/Web/API/DeviceMotionEvent
 
 $(document).ready(function(){
-  var city = "philadelphia";
-  var country =  "usa";
-  var url = "//api.openweathermap.org/data/2.5/weather?q="+city+","+country+"&APPID=dfc12af78c0d3f30f389d0cc934567db"
 
-  $.getJSON(url,function(data){
-          console.log(JSON.stringify(data));
+  window.addEventListener('devicemotion', function(event) {
+    $('.acX').html(event.acceleration.x);
   });
+
 });
