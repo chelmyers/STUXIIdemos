a0ebbba68c92bc81180aaa7fb7c91a886c7b6972
Chelsea Myers <chel.myers@gmail.com>
2016-04-27 10:49:57 -0400

    weather: ajax instead of getJSON

--- wk05/01-geolocation/a/js/app.js
+++ wk05/01-geolocation/a/js/app.js
@@ -3,9 +3,11 @@
 
 //openweathermap
 //http://openweathermap.org/current#geo
-console.log(1);
+
 
 $(document).ready(function(){
+  console.log(1);
+  
   if ("geolocation" in navigator) {
     console.log("Geolocation is available");
     navigator.geolocation.getCurrentPosition(success, error);
@@ -19,14 +21,23 @@ function success(position) {
   var c = position.coords;
   var t = position.timestamp;
   // $('.location').html(c.latitude + " latitude and " + c.longitude + " longitude");
-  console.log("Location displayed");
+  console.log("success");
 
-  $.getJSON(
-      "http:api.openweathermap.org/data/2.5/weather?lat=" + c.latitude + "&lon=" + c.longitude,
-      function(json){
-        console.log(JSON.stringify(json));
-        console.log("weather displayed");
-      });
+  var url = "http:api.openweathermap.org/data/2.5/weather?lat=" + c.latitude + "&lon=" + c.longitude + "&callback=?";
+
+  $.ajax({
+       type: "POST",
+       dataType: "jsonp",
+       url: url,
+       async: false,
+       success: function (data) {
+           var temp = data.list[0].main.temp;
+           console.log(temp);
+       },
+       error: function (errorData) {
+           alert("Error while getting weather: " + errorData.status);
+       }
+   });
 }
 
 function error() {
