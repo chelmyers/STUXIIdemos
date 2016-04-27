ac9f5ef8e747eeae09c6ffe6aee3bc4b5065abd0
Chelsea Myers <chel.myers@gmail.com>
2016-04-27 13:37:43 -0400

    shake: remove example to be done in class

--- wk05/05-shake/a/js/app.js
+++ wk05/05-shake/a/js/app.js
@@ -1,18 +1,1 @@
-//https://developer.mozilla.org/en-US/docs/Web/API/DeviceMotionEvent
-
-$(document).ready(function(){
-
-  var myShakeEvent = new Shake({
-      threshold: 10 //strength
-      //another option in timeout for frequency of event
-  });
-
-  myShakeEvent.start();
-
-  window.addEventListener('shake', shakeOccurred, false);
-
-  function shakeOccurred() {
-      alert('You shook your phone!');
-  }
-
-});
+//https://github.com/alexgibson/shake.js/
