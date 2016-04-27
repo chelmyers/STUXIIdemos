bc4d31ba304e35d3908c9b399d366b805efc049a
Chelsea Myers <chel.myers@gmail.com>
2016-04-27 12:45:50 -0400

    map: combine with geolocation

--- wk05/02-googlemap/a/js/app.js
+++ wk05/02-googlemap/a/js/app.js
@@ -1,6 +1,23 @@
-//Google maps
-//https://developers.google.com/maps/documentation/javascript/tutorial#HTML5
-//https://developers.google.com/maps/documentation/javascript/examples/
+$(document).ready(function(){
+  console.log(1);
+  if ("geolocation" in navigator) {
+    console.log("Geolocation is available");
+    navigator.geolocation.getCurrentPosition(success, error);
+  } else {
+    alert("Geolocation is not available or is disabled on this device.");
+  }
+});
+
+
+function success(position) {
+  var c = position.coords;
+  var myLatLong = new google.maps.LatLng(c.latitude, c.longitude);
+  createMap(myLatLong);
+}
+
+function error() {
+  alert("There has been an error finding your location.");
+}
 
 function createMap(city) {
   var mapOptions = {
@@ -13,30 +30,4 @@ function createMap(city) {
 
   var map = new google.maps.Map(document.getElementById("map"),mapOptions);
 
-}
-
-
-
-// HTML5 Geolocation
-//https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/Using_geolocation
-
-$(document).ready(function(){
-  if ("geolocation" in navigator) {
-    console.log("Geolocation is available");
-    navigator.geolocation.getCurrentPosition(success, error);
-  } else {
-    alert("Geolocation is not available or is disabled on this device.");
-  }
-});
-
-
-function success(position) {
-  var c = position.coords;
-  var myLatLong = new google.maps.LatLng(c.lat, c.long);
-  createMap(myLatLong);
-
-}
-
-function error() {
-  alert("There has been an error finding your location.");
 }
