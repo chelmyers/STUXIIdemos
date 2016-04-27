17839754284b743aa2be4f170c215869cbc6ebc3
Chelsea Myers <chel.myers@gmail.com>
2016-04-27 10:40:13 -0400

    weather: debug msgs

--- wk05/01-geolocation/a/js/app.js
+++ wk05/01-geolocation/a/js/app.js
@@ -17,12 +17,14 @@ $(document).ready(function(){
 function success(position) {
   var c = position.coords;
   var t = position.timestamp;
-  $('.location').html(c.latitude + " latitude and " + c.longitude + " longitude");
+  // $('.location').html(c.latitude + " latitude and " + c.longitude + " longitude");
   console.log("Location displayed");
 
   $.getJSON("http:api.openweathermap.org/data/2.5/weather?lat=" + c.latitude + "&lon=" + c.longitude,function(json){
       console.log(JSON.stringify(json));
+      console.log("weather displayed");
   });
+
 }
 
 function error() {
