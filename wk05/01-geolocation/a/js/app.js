e7d045e5183426dd6cb6777162ec7670ddf670ef
Chelsea Myers <chel.myers@gmail.com>
2016-04-27 09:23:19 -0400

    geo: clean up example

--- wk05/01-geolocation/a/js/app.js
+++ wk05/01-geolocation/a/js/app.js
@@ -1,27 +1,19 @@
 // HTML5 Geolocation
-// https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/Using_geolocation
-
+//https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/Using_geolocation
 
 $(document).ready(function(){
-
-
   if ("geolocation" in navigator) {
     console.log("Geolocation is available");
-
     navigator.geolocation.getCurrentPosition(success, error);
-
   } else {
-
+    alert("Geolocation is not available or is disabled on this device.");
   }
-
-
 });
 
 
 function success(position) {
   var c = position.coords;
-
-  $('.location').html(c.latitude + " latitude and " + c.longitude + "longitude");
+  $('.location').html(c.latitude + " latitude and " + c.longitude + " longitude");
   console.log("Location displayed");
 }
 
