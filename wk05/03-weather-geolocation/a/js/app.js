572a5a37bad7e7c4572b15c95a631c980da40b25
Chelsea Myers <chel.myers@gmail.com>
2016-04-27 11:31:24 -0400

    weather: simple example

---
+++ wk05/03-weather-geolocation/a/js/app.js
@@ -0,0 +1,39 @@
+// HTML5 Geolocation
+//https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/Using_geolocation
+
+//openweathermap
+//http://openweathermap.org/current#geo
+
+
+$(document).ready(function(){
+  if ("geolocation" in navigator) {
+    console.log("Geolocation is available");
+    navigator.geolocation.getCurrentPosition(success, error);
+  } else {
+    alert("Geolocation is not available or is disabled on this device.");
+  }
+});
+
+
+function success(position) {
+  var c = position.coords;
+  var t = position.timestamp;
+  console.log("Location successfully retrieved");
+
+  var url = "//api.openweathermap.org/data/2.5/weather?lat=" + c.latitude + "&lon=" + c.longitude + "&APPID=dfc12af78c0d3f30f389d0cc934567db";
+
+  //Pretty Print JSON
+  //http://jsonprettyprint.com/
+
+  $.getJSON(url,function(data){
+          console.log(JSON.stringify(data));
+          console.log(data.weather[0].description);
+          console.log(data.name);
+          $('.weather').html(data.weather[0].description);
+          $('.city').html(data.name);
+  });
+}
+
+function error() {
+  alert("There has been an error finding your location.");
+}
