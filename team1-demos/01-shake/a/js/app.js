1be571d7d7c3ee547dd4234d4b6902e9ec31ed84
Chelsea Myers <chel.myers@gmail.com>
2016-05-17 17:40:50 -0400

    shake js

--- team1-demos/01-shake/a/js/app.js
+++ team1-demos/01-shake/a/js/app.js
@@ -1,1 +1,17 @@
 //https://github.com/alexgibson/shake.js/
+
+var myShakeEvent = new Shake({
+    threshold: 15, // optional shake strength threshold
+    timeout: 1000 // optional, determines the frequency of event generation
+});
+
+myShakeEvent.start();
+
+window.addEventListener('shake', shakeEventDidOccur, false);
+
+//function to call when shake occurs
+function shakeEventDidOccur () {
+
+    //put your own code here etc.
+    alert('shake!');
+}
